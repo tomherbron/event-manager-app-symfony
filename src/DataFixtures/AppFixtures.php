841927d7ec8e7922b9ed23107c13f55622a8897b
Bounsouane <n.nanteau@gmail.com>
2023-06-06 13:40:12 +0200

    Fixtures + Affichage

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -18,12 +18,10 @@ use Symfony\Component\Validator\Constraints\DateTime;
 class AppFixtures extends Fixture
 {
     private UserPasswordHasherInterface $encoder;
-    private CampusRepository $repository;
 
     //injection de dépendance
     public function __construct(UserPasswordHasherInterface $encoder, CampusRepository $repository){
         $this->encoder = $encoder;
-        $this->repository = $repository;
     }
 
     public function load(ObjectManager $manager): void
@@ -44,9 +42,9 @@ class AppFixtures extends Fixture
 
         for ($i=0;$i<15;$i++){
             $ville=new Ville();
-            $ville
-                ->setNom($generator->ville)
-                ->setCodePostal($generator->randomNumer(5,true));
+
+            $ville->setNom($generator->city);
+            $ville->setCodePostal($generator->randomNumber(5,true));
             $manager->persist($ville);
         }
 
@@ -56,17 +54,20 @@ class AppFixtures extends Fixture
 
             $user=new Utilisateur();
 
-            $user->setUsername($generator->username);
-            $user->setNom($generator->nom);
-            $user->setPrenom($generator->prenom);
-            $user->setTelephone($generator->telephone);
+
+            $user->setUsername($generator->name);
+            $user->setNom($generator->firstName);
+            $user->setPrenom($generator->lastName);
+            $user->setTelephone($generator->phoneNumber);
             $user->setEmail($generator->email);
             $user->setActif(true);
 
             $user->setCampus($campus);
 
+
+
             //Mot de Passe
-            $user->setPassword($generator->$this->hasher->hashPassword($user, 'test123456'));
+            //$user->setPassword($generator->$this->hasher->hashPassword($user, 'test123456'));
 
             $plainPassword = "123456";
             $encoded = $this->encoder->hashPassword($user, $plainPassword);
@@ -81,10 +82,10 @@ class AppFixtures extends Fixture
             $lieu = new Lieu();
             $lieu
                 ->setLatitude($generator->latitude)
-                ->setNom($generator->nom)
+                ->setNom($generator->name)
                 ->setLongitude($generator->longitude)
-                ->setRue($generator->rue)
-                ->setVille($generator->ville);
+                ->setRue($generator->streetName)
+                ->setVille($ville);
 
             $manager->persist($lieu);
         }
@@ -106,108 +107,18 @@ class AppFixtures extends Fixture
                 ->setDateHeureDebut($generator->dateTime)
                 ->setDuree($generator->randomNumber(4,false))
                 ->setInfos($generator->realText(200))
-                ->setLieu($generator->lieu)
-                ->setNom($generator->nom)
+                ->setLieu($lieu)
+                ->setNom($generator->name)
                 ->setNbMaxInscriptions($generator->randomNumber(3,false))
-                ->setEtat($etat);
-            $manager->persist(($sortie));
+                ->setEtat($etat)
+                ->setOrganisateur($user);
+
+            $manager->persist($sortie);
         }
 
         $manager->flush();
 
     }
+
+
 }
-//    private UserPasswordHasherInterface $encoder;
-//    private CampusRepository $repository;
-//
-//    public function __construct(UserPasswordHasherInterface $encoder, CampusRepository $repository){
-//        $this->encoder = $encoder;
-//        $this->repository = $repository;
-//    }
-//
-//    public function load(ObjectManager $manager): void
-//    {
-//        $user = new Utilisateur();
-//        $user->setUsername('julien');
-//        $user->setNom('Chéreau');
-//        $user->setPrenom('Julien');
-//        $user->setTelephone('0233289763');
-//        $user->setEmail('julien@gmail.com');
-//        $user->setActif(true);
-//
-//        $campus = $this->repository->find('1');
-//
-//        $user->setCampus($campus);
-//        $plainPassword = '123456';
-//        $encoded = $this->encoder->hashPassword($user, $plainPassword);
-//        $user->setPassword($encoded);
-//
-//        $manager->persist($user);
-//
-//        $manager->flush();
-//    }
-//
-//    public function addSorties(ObjectManager $manager)
-//    {
-//        //Test sortieA
-//        $sortieA = new Sortie();
-//        $sortieA
-//            ->setNom('testA')
-//            ->setDuree(123)
-//            ->setNbMaxInscriptions(123)
-//            ->setInfos('testA');
-//
-//        $h="08/08/2023 14:48";
-//        $hdebut= \DateTime::createFromFormat('d/m/Y H:i',$h);
-//        $sortieA->setDateHeureDebut($hdebut);
-//
-//        $d="15/08/2023";
-//        $dInscription= \DateTime::createFromFormat('d/m/Y', $d);
-//        $sortieA->setDateLimiteInscription($dInscription);
-//
-//        $organisateur = $this->repository->find('1');
-//        $sortieA->setOrganisateur($organisateur);
-//
-//        $campus = $this->repository->find('1');
-//        $sortieA->setCampus($campus);
-//
-//        $etat = $this->repository->find('1');
-//        $sortieA->setEtat($etat);
-//
-//        $lieu = $this->repository->find('2');
-//        $sortieA->setLieu($lieu);
-//
-//        $manager->persist($sortieA);
-//
-//        $manager->flush();
-//
-//        //Test sortieB
-//        $sortieB = new Sortie();
-//        $sortieB
-//            ->setNom('testB')
-//            ->setDuree(123)
-//            ->setNbMaxInscriptions(123)
-//            ->setInfos('testB');
-//
-//        $h="09/09/2023";
-//        $dInscription= \DateTime::createFromFormat('d/m/Y',$h);
-//        $sortieA->setDateLimiteInscription($dInscription);
-//
-//        $organisateur = $this->repository->find('1');
-//        $sortieA->setOrganisateur($organisateur);
-//
-//        $campus = $this->repository->find('1');
-//        $sortieA->setCampus($campus);
-//
-//        $etat = $this->repository->find('1');
-//        $sortieA->setEtat($etat);
-//
-//        $lieu = $this->repository->find('2');
-//        $sortieA->setLieu($lieu);
-//
-//        $manager->persist($sortieA);
-//
-//        $manager->flush();
-//
-//    }
-//}
